ef454c8b9e54fc2f3b292c6f5070c917db4ad4fc
SaffiSardar <saffisardar2012@gmail.com>
2024-08-23 03:08:41 -0700

    created all tables

--- Frontend/src/pages/Warehouses.jsx
+++ Frontend/src/pages/Warehouses.jsx
@@ -6,7 +6,50 @@ const Warehouses = () => {
     <div className="container">
       <SearchBox/>
       <div className="table">
-        
+      <table>
+          <tr>
+            <th>ID</th>
+            <th>Location</th>
+            <th>Landline No.</th>
+            <th>Postal Code</th>
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td> 
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td> 
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td> 
+          </tr>
+        </table>
       </div>
     </div>
   )
--- Frontend/src/pages/WeatherData.jsx
+++ Frontend/src/pages/WeatherData.jsx
@@ -7,7 +7,64 @@ const WeatherData = () => {
     <div className="container">
       <SearchBox/>
       <div className="table">
-        
+      <table>
+          <tr>
+            <th>ID</th>
+            <th>Date</th>
+            <th>Weather</th>
+            <th>Wind Speed</th>
+            <th>Humidity</th>
+            <th>Pressure</th>
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>  
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>  
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>  
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>  
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>  
+          </tr>
+          <tr>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>
+            <td>xyz</td>  
+          </tr>
+        </table>
       </div>
     </div>
   )
