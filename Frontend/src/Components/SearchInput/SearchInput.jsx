7a609bd14aabeadb91a1fdb6f0ffded9a8e5301e
SaffiSardar <saffisardar2012@gmail.com>
2024-08-23 00:48:44 -0700

    creating table of sr

---
+++ Frontend/src/Components/SearchInput/SearchInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SearchInput.css'
+
+const SearchInput = () => {
+  return (
+    <input type="text" name="" className='input' placeholder='ID' />
+  )
+}
+
+export default SearchInput
--- Frontend/src/pages/CustomerRecords.jsx
+++ Frontend/src/pages/CustomerRecords.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import './CustomerRecords.css'
+import Button from '../Components/Button/Button'
+import SearchInput from '../Components/SearchInput/SearchInput'
 
 const CustomerRecords = () => {
   return (
     <div className='container'>
       <div className="searchbox">
-        
+        <SearchInput/>
+        <Button/>
       </div>
-      <table className='table'>
+        <div className='table'>
 
-      </table>
+        <table>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email Address</th>
+          </tr>
+          <tr>
+            <td>Hillary</td>
+            <td>Nyakundi</td>
+            <td>tables@mail.com</td>
+          </tr>
+          <tr>
+            <td>Lary</td>
+            <td>Mak</td>
+            <td>developer@mail.com</td>
+          </tr>
+        </table>
+      </div>
     </div>
   )
 }
